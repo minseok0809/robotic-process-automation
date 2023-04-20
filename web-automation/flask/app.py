4e592b75a4e653bc651c7575f5a4bc31ec76aa1d
minseok0809 <minseokyang0809@gmail.com>
2023-04-20 21:14:37 +0900

    feat: web automation

--- web-automation/flask/app.py
+++ web-automation/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, session, app
 import pandas as pd 
 import pickle5 as pickle
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,7 +8,11 @@ import random
 
 app = Flask(__name__)
 
-
+@app.before_request
+def make_session_permanent():
+    app.secret_key = 'secretkey'
+    session.permanent = True
+    
 @app.route('/', methods=['GET', 'POST'])
 def predict():
 
@@ -34,145 +38,136 @@ def predict():
 	# joblib.dump(clf, 'NB_spam_model.pkl')
 	# NB_spam_model = open('NB_spam_model.pkl','rb')
 	# clf = joblib.load(NB_spam_model)
+	
 
 	if request.method == 'POST':
 		try:
-			message_task1 = request.form['message_task1']
-			data = [message_task1]
+			message_out_of_order = request.form['message_out_of_order']
+			data = [message_out_of_order]
 			vect = cv.transform(data).toarray()
 			prediction = clf.predict(vect)
-			number_task1 = (prediction - random.randrange(1, 11))[0]
+			number_out_of_order = (prediction - random.randrange(1, 11))[0]
 			if prediction == 1:
-				result_task1 = "spam"
+				result_out_of_order = "spam"
 			elif prediction == 0:
-				result_task1 = "Not a Spam" + "(It is a Wham)"
+				result_out_of_order = "Not a Spam" + "(It is a Wham)"
 		
 		except KeyError:
 			pass
 
 		try:
-			message_task2 = request.form['message_task2']
-			data = [message_task2]
+			message_caution = request.form['message_caution']
+			data = [message_caution]
 			vect = cv.transform(data).toarray()
 			prediction = clf.predict(vect)
-			number_task2 = (prediction - random.randrange(1, 11))[0]
-			number_task2_01=number_task2+1
-			number_task2_02=number_task2+2
+			number_caution_01 = (prediction - random.randrange(1, 11))[0]
+			number_caution_02 = number_caution_01+1
+			number_caution_03 = number_caution_01+2
 			if prediction == 1:
-				result_task2 = "spam"
+				result_caution_01 = "spam"
+				result_caution_02 = "spam"
+				result_caution_03 = "spam"
 			elif prediction == 0:
-				result_task2 = "Not a Spam" +"(It is a Bam)"
-				result_task2_01="Not a Spam"+"(It is a Ham)"
-				result_task2_02="Not a Spam"+"(It is a Zam)"
+				result_caution_01 = "Not a Spam" +"(It is a Bam)"
+				result_caution_02 = "Not a Spam"+"(It is a Ham)"
+				result_caution_03 = "Not a Spam"+"(It is a Zam)"
 
 		except KeyError:
 			pass
 		
 		try:	
-			message_task3 = request.form['message_task3']
-			data = [message_task3]
+			message_take_measure = request.form['message_take_measure']
+			data = [message_take_measure]
 			vect = cv.transform(data).toarray()
 			prediction = clf.predict(vect)
-			number_task3 = (prediction - random.randrange(1, 11))[0]
-			number_task3_01=number_task3+4
-			number_task3_02=number_task3+5
+			number_take_measure_01 = (prediction - random.randrange(1, 11))[0]
+			number_take_measure_02 = number_take_measure_01+4
+			number_take_measure_03 = number_take_measure_01+5
 
 			if prediction == 1:
-				result_task3 = "spam"
+				result_take_measure_01 = "spam"
+				result_take_measure_02 = "spam"
+				result_take_measure_03 = "spam"
 			elif prediction == 0:
-				result_task3 = "Not a Spam" +"(It is a Sam)"
-				result_task3_01="Not a Spam" +"(It is a Pam)"
-				result_task3_02="Not a Spam" +"(It is a Fam)"
+				result_take_measure_01 = "Not a Spam" +"(It is a Sam)"
+				result_take_measure_02 = "Not a Spam" +"(It is a Pam)"
+				result_take_measure_03 = "Not a Spam" +"(It is a Fam)"
 		
 		except KeyError:
 			pass
 
 		finally:
-
 			try:
-				result_task1, number_task1
+				result_out_of_order, number_out_of_order
+				session['result_out_of_order'] = result_out_of_order
+				session['number_out_of_order'] = int(number_out_of_order)
 
 			except NameError:
-				result_task1 = ""
-				number_task1 = ""
 				try:
-					with open("variables_task1.txt", "r") as f:
-						lines = f.readlines()
-						result_task1 = lines[0].strip(); number_task1 = lines[1].strip()
-				except IndexError: 
-					pass
+					result_out_of_order = session['result_out_of_order']
+					number_out_of_order = session['number_out_of_order']
+				except KeyError:
+					result_out_of_order = number_out_of_order = ""
 
 			try:
-				result_task2, number_task2, \
-				result_task2_01, number_task2_01, \
-				result_task2_02, number_task2_02
+				result_caution_01, number_caution_01,
+				result_caution_02, number_caution_02,
+				result_caution_03, number_caution_03
+				session['result_caution_01'] = result_caution_01
+				session['number_caution_01'] = int(number_caution_01)
+				session['result_caution_02'] = result_caution_02
+				session['number_caution_02'] = int(number_caution_02)
+				session['result_caution_03'] = result_caution_03
+				session['number_caution_03'] = int(number_caution_03)
 
 			except NameError:
-				result_task2 = ""; number_task2= ""
-				result_task2_01= ""; number_task2_01= ""
-				result_task2_02= ""; number_task2_02= ""
 				try:
-					with open("variables_task2.txt", "r") as f:
-						lines = f.readlines()
-						result_task2 = lines[0].strip(); number_task2 = lines[1].strip()
-						result_task2_01 = lines[2].strip(); number_task2_01 = lines[3].strip()
-						result_task2_02 = lines[4].strip(); number_task2_02 = lines[5].strip()
-				except IndexError: 
-					pass
+					result_caution_01 = session['result_caution_01']
+					number_caution_01 = session['number_caution_01']
+					result_caution_02 = session['result_caution_02']
+					number_caution_02 = session['number_caution_02']
+					result_caution_03 = session['result_caution_03']
+					number_caution_03 = session['number_caution_03']				
+				except KeyError:
+					result_caution_01 = number_caution_01 = \
+					result_caution_02 = number_caution_02 =  \
+					result_caution_03 = number_caution_03 = ""
 
 			try:
-				result_task3, number_task3, \
-				result_task3_01, number_task3_01, \
-				result_task3_02, number_task3_02
+				result_take_measure_01, number_take_measure_01,
+				result_take_measure_02, number_take_measure_02,
+				result_take_measure_03, number_take_measure_03
+				session['result_take_measure_01'] = result_take_measure_01
+				session['number_take_measure_01'] = int(number_take_measure_01)
+				session['result_take_measure_02'] = result_take_measure_02
+				session['number_take_measure_02'] = int(number_take_measure_02)
+				session['result_take_measure_03'] = result_take_measure_03
+				session['number_take_measure_03'] = int(number_take_measure_03)
 
 			except NameError:
-				result_task3 = ""; number_task3= "" 
-				result_task3_01= ""; number_task3_01= ""
-				result_task3_02= ""; number_task3_02= ""
 				try:
-					with open("variables_task3.txt", "r") as f:
-						lines = f.readlines()
-						result_task3 = lines[0].strip(); number_task3 = lines[1].strip()
-						result_task3_01 = lines[2].strip(); number_task3_01 = lines[3].strip()
-						result_task3_02 = lines[4].strip(); number_task3_02 = lines[5].strip()
-				except IndexError: 
-					pass
+					result_take_measure_01 = session['result_take_measure_01']
+					number_take_measure_01 = session['number_take_measure_01']
+					result_take_measure_02 = session['result_take_measure_02']
+					number_take_measure_02 = session['number_take_measure_02']
+					result_take_measure_03 = session['result_take_measure_03']
+					number_take_measure_03 = session['number_take_measure_03']				
+				except KeyError:
+					result_take_measure_01 = number_take_measure_01 = \
+					result_take_measure_02 = number_take_measure_02 = \
+					result_take_measure_03 = number_take_measure_03 = ""
 
-		task1_list = [result_task1, number_task1]
-		task2_list = [result_task2, number_task2, result_task2_01, number_task2_01,
-		  result_task2_02, number_task2_02]
-		task3_list = [result_task3, number_task3, result_task3_01, number_task3_01,
-		  result_task3_02, number_task3_02]
+		return render_template('index.html', result_out_of_order=result_out_of_order, number_out_of_order=number_out_of_order,
+			 result_caution_01=result_caution_01, number_caution_01=number_caution_01,
+			 result_caution_02=result_caution_02, number_caution_02=number_caution_02,
+			 result_caution_03=result_caution_03, number_caution_03=number_caution_03,
+			 result_take_measure_01=result_take_measure_01, number_take_measure_01=number_take_measure_01,
+			 result_take_measure_02=result_take_measure_02, number_take_measure_02=number_take_measure_02,
+			 result_take_measure_03=result_take_measure_03, number_take_measure_03=number_take_measure_03)
+	
+	session.clear()
 
-		with open("variables_task1.txt", "w") as f:
-			f.write("\n".join(map(str, task1_list)))
-
-		with open("variables_task2.txt", "w") as f:
-			f.write("\n".join(map(str, task2_list)))
-
-		with open("variables_task3.txt", "w") as f:
-			f.write("\n".join(map(str, task3_list)))
-
-		return render_template('index.html',
-		result_task1=result_task1, number_task1=number_task1,
-	result_task2=result_task2, number_task2=number_task2,
-	result_task2_01=result_task2_01, number_task2_01=number_task2_01,
-	result_task2_02=result_task2_02, number_task2_02=number_task2_02,
-	result_task3=result_task3, number_task3=number_task3,
-	result_task3_01=result_task3_01, number_task3_01=number_task3_01,
-	result_task3_02=result_task3_02, number_task3_02=number_task3_02)
-
-	with open("variables_task1.txt", "w") as f:
-		f.write("")
-
-	with open("variables_task2.txt", "w") as f:
-		f.write("")
-
-	with open("variables_task3.txt", "w") as f:
-		f.write("")
-		
 	return render_template('index.html')
-
 
 
 if __name__ == '__main__':
